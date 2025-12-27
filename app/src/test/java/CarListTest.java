0012a85aa73f48a52c762bce47fb8ce12047981b
9hokage4 <claymamedov206@gmail.com>
2025-12-27 20:44:47 +0300

    first init @BeforeEach

--- app/src/test/java/CarListTest.java
+++ app/src/test/java/CarListTest.java
@@ -1,4 +1,5 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -8,15 +9,19 @@ public class CarListTest {
 
     @BeforeEach
     public void setUp() throws Exception {
-        // init
+        for (int i = 0; i < 100; i++) {
+            carList.add(new Car("Brand" + i, "Name" + i));
+        }
     }
 
     @Test
     public void whenAdded100ElementsThenSizeMustBe100() {
-        for (int i = 0; i < 100; i++) {
-            carList.add(new Car("Brand" + i, "Name" + i));
-        }
-
         assertEquals(100, carList.size());
+    }
+
+    @Test
+    public void whenElementRemoveByIndexThenSizeMustBeDecreased() {
+        assertTrue(carList.removeAt(5));
+        assertEquals(99, carList.size());
     }
 }
