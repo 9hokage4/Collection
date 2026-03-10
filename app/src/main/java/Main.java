097dffbcdc9c386a7c4998975a3d3d91e417f951
9hokage4 <claymamedov206@gmail.com>
2026-03-10 14:24:15 +0300

    Try to use TreeSet

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -2,14 +2,20 @@ import justcouse.Car;
 import justcouse.CarArrayList;
 import justcouse.CarCollection;
 
+import java.util.Set;
+import java.util.TreeSet;
+
+import static java.lang.Math.*;
+
 public class Main {
     public static void main(String[] args) {
-        CarCollection cars = new CarArrayList();
-        for (int i = 0; i < 10; i++) {
-            cars.add(new Car("Brand" + i, i));
+        Set<Integer> numbers = new TreeSet<>();
+        for (int i = 0; i < 100; i++){
+            numbers.add((int)(Math.random() * 10));
         }
-        for (Car car : cars) {
-            System.out.println(car.getBrand() + " " + car.getNumber());
+        for (int number : numbers){
+            System.out.println(number);
         }
+
     }
 }
